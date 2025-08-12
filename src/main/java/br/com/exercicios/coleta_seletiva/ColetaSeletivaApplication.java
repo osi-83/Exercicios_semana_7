4d8c67e7f700ba7575b60cd041ab7f50f555366c
Osiel Fernandes <osiel.fernandes.1983@gmail.com>
2025-08-12 18:14:53 -0300

    Created repository for week 7

---
+++ src/main/java/br/com/exercicios/coleta_seletiva/ColetaSeletivaApplication.java
@@ -0,0 +1,13 @@
+package br.com.exercicios.coleta_seletiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColetaSeletivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColetaSeletivaApplication.class, args);
+	}
+
+}
